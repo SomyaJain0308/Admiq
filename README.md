708655ee977a904233edd5337de8ec1b63cb47ae
SomyaJain0308 <somyaj0308@gmail.com>
2026-08-08 06:41:56 +0000

    Added api to view students

--- README.md
+++ README.md
@@ -1,18 +1,2 @@
-source backend/.venv/bin/activate
-export PROMETHEUS_MULTIPROC_DIR=/tmp/prometheus_multiproc
-mkdir -p /tmp/prometheus_multiproc
-rm -f /tmp/prometheus_multiproc/*   # clear stale data from previous runs
-fastapi dev backend/main.py
-
-
-
-source backend/.venv/bin/activate
-export PROMETHEUS_MULTIPROC_DIR=/tmp/prometheus_multiproc
-celery -A backend.backgroundTasks.celery_app worker --loglevel=info --concurrency=1
-
-
-
-source backend/.venv/bin/activate
-cd backend
-curl -X POST "http://127.0.0.1:8000/router/colleges/1/documents" -F "uploaded_by=1" -F "file=@test.pdf"
-curl "http://127.0.0.1:8000/router/colleges/1/documents/1"
+2) Add the update student profile feature to agent.py inside rag/
+3) Save message from faculty and send it to students.
--- backend/app/api/v1/routers/students.py
+++ backend/app/api/v1/routers/students.py
@@ -3,9 +3,28 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
 from backend.app.database import get_db
+from backend.app.models.Student import Student
 from backend.app.models.Message import Message
 
-router = APIRouter()
+router = APIRouter(tags=["Students"], prefix="/students")
+
+
+@router.get("{college_id}")
+async def get_students(college_id: int, db: AsyncSession = Depends(get_db)):
+    students_result = await db.execute(select(Student).where(Student.college_id == college_id))
+    students = students_result.scalars().all()
+    if not students:
+        raise HTTPException(status_code=404, detail="No students found for this college")
+    return students
+
+
+@router.get("{college_id}/{student_id}")
+async def get_student(college_id: int, student_id: int, db: AsyncSession = Depends(get_db)):
+    student_result = await db.execute(select(Student).where(Student.college_id == college_id, Student.id == student_id).limit(1))
+    student = student_result.scalars().first()
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
 
 
 @router.get("/view_convo/{college_id}/{student_id}")
